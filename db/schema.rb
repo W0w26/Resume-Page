4b8c5a551726f9a17be8a1c5c490c17e5001f020
W0w26 <taylan7ozmergen@gmail.com>
2024-08-14 14:17:14 +0300

    Deploying the website on render

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,12 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_08_13_142856) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.text "body"
-    t.integer "contact_message_id"
+    t.bigint "contact_message_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["contact_message_id"], name: "index_comments_on_contact_message_id"
