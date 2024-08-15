1ffba37a83fde0eb102126ff04200e51d060ecb8
W0w26 <taylan7ozmergen@gmail.com>
2024-08-15 15:48:08 +0300

    Fixes

--- app/controllers/user/omniauth_callbacks_controller.rb
+++ app/controllers/user/omniauth_callbacks_controller.rb
@@ -8,7 +8,7 @@ class User::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       user = User.from_omniauth(auth)
 
       if user.present? && user.persisted?
-        refresh_google_token(user)
+        #refresh_google_token(user)
         sign_out_all_scopes
         flash[:success] = t 'devise.omniauth_callbacks.success', kind: 'Google'
         sign_in_and_redirect user, event: :authentication
