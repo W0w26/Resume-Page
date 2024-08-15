2e3ae6b4205e002341232575660cac4f3aae3b22
W0w26 <taylan7ozmergen@gmail.com>
2024-08-15 14:26:12 +0300

    Session handling

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :cookie_store, key: '_myapp_session', secure: Rails.env.production?,expire_after: 1.hour
+#Rails.application.config.session_store :cookie_store, key: '_myapp_session', secure: Rails.env.production?,expire_after: 1.hour
--- lib/middleware/session_timeout.rb
+++ lib/middleware/session_timeout.rb
@@ -1,21 +1,21 @@
-class SessionTimeout
-  def initialize(app)
-    @app = app
-  end
-
-  def call(env)
-    request = Rack::Request.new(env)
-    last_seen_at = request.session[:last_seen_at]
-
-    if last_seen_at
-      last_seen_at = Time.parse(last_seen_at) rescue nil
-      if last_seen_at && Time.current - last_seen_at > 1.hour
-        request.session.clear
-        return [302, { 'Location' => '/users/sign_in' }, []]
-      end
-    end
-
-    request.session[:last_seen_at] = Time.current.iso8601
-    @app.call(env)
-  end
-end
+#class SessionTimeout
+#   def initialize(app)
+#     @app = app
+#   end
+#
+#   def call(env)
+#     request = Rack::Request.new(env)
+#     last_seen_at = request.session[:last_seen_at]
+#
+#     if last_seen_at
+#       last_seen_at = Time.parse(last_seen_at) rescue nil
+#       if last_seen_at && Time.current - last_seen_at > 1.hour
+#         request.session.clear
+#         return [302, { 'Location' => '/users/sign_in' }, []]
+#       end
+#     end
+#
+#     request.session[:last_seen_at] = Time.current.iso8601
+#     @app.call(env)
+#   end
+# end
