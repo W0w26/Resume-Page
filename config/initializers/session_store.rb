26c0d54a1a701496bcfdd8f64b3e0829ab4a897e
W0w26 <taylan7ozmergen@gmail.com>
2024-08-15 14:15:57 +0300

    Session handling

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,2 +1,1 @@
-Rails.application.config.session_store :cookie_store, key: '_myapp_session', secure: Rails.env.production?
-Rails.application.config.session_store :cookie_store, key: '_your_app_session', expire_after: 1.hour
+Rails.application.config.session_store :cookie_store, key: '_myapp_session', secure: Rails.env.production?,expire_after: 1.hour
